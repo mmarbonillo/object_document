3e80a46105a9d66265c282996a46a5860386a81c
mmarbonillo <mbonillo95@gmail.com>
2019-10-07 22:39:13 +0200

    Ejercicios hechos

--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -2,5 +2,4 @@
 1. Añade un párrafo nuevo en el apartado de sipnosis que las letras sean de color azul
 2. Recoge todos los elementos de clase "apartado" y añádele un nombre que se llamen igual
 3. Recoge el <div> de id = "pie", comprueba si tiene nodos hijos y si no tiene, que se añada uno (un enlace a google).
-4. 
 */
