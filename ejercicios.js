b9851a66fa099dd559bbd3574c31cfdc3f326c82
mmarbonillo <mbonillo95@gmail.com>
2019-10-07 13:32:32 +0200

    first commit

---
+++ ejercicios.js
@@ -0,0 +1,5 @@
+/*
+1. Añade un párrafo nuevo en el apartado de sipnosis que sea de color azul
+2. Recoge todos los elementos de clase "apartado" y añádele un nombre que se llamen igual
+3. Recoge el <div> de id = "pie", comprueba si tiene nodos hijos y si no tiene, que se añada uno.
+*/
